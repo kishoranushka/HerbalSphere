59f9c6998a5431bce089c0671038ed3b2c1756a8
Abhishek Mewada <abhishek.engineer3@gmail.com>
2024-09-21 09:59:26 +0530

    Environment Setup

--- server/src/server.js
+++ server/src/server.js
@@ -1,8 +1,9 @@
 import express from 'express';
+import { config } from './config/config.js';
 
 const app = express();
 
-const PORT = 3000;
+const PORT = config.port || 3000;
 
 app.listen(PORT, () => {
 	console.log(`Listining on port: ${PORT}`);
