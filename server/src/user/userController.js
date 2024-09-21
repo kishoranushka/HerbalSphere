9ec6beafbe85c20c8af8ba340cbf10e97cbd736b
Abhishek Mewada <abhishek.engineer3@gmail.com>
2024-09-21 21:00:27 +0530

    add login end point

--- server/src/user/userController.js
+++ server/src/user/userController.js
@@ -3,6 +3,7 @@ import createHttpError from 'http-errors';
 import userModel from './userModel.js';
 import bcrypt from 'bcrypt';
 import pkg from 'jsonwebtoken';
+import { model } from 'mongoose';
 const { sign } = pkg;
 
 const createUser = async (req, res, next) => {
@@ -41,7 +42,7 @@ const createUser = async (req, res, next) => {
 	}
 
 	try {
-		// Token generation --> JWT
+		// Generate JWT token
 		const token = sign({ sub: newUser._id }, config.jwtSecret, {
 			expiresIn: '7d',
 			algorithm: 'HS256', // by default
@@ -54,4 +55,48 @@ const createUser = async (req, res, next) => {
 	}
 };
 
-export { createUser };
+const loginUser = async (req, res, next) => {
+	const { email, password } = req.body;
+
+	if (!email || !password) {
+		const error = createHttpError(400, 'All fields are required');
+		return next(error);
+	}
+
+	// Find user by email
+	let user;
+	try {
+		user = await userModel.findOne({ email });
+		if (!user) {
+			return next(createHttpError(404, 'User not found'));
+		}
+	} catch {
+		return next(createHttpError(500, 'Error while fetching user'));
+	}
+
+	// Compare passwords
+	let isMatch;
+	try {
+		isMatch = await bcrypt.compare(password, user.password);
+		if (!isMatch) {
+			return next(createHttpError(400, 'Username or password incorrect!'));
+		}
+	} catch {
+		return next(createHttpError(500, 'Error while comparing passwords'));
+	}
+
+	// Generate JWT token
+	try {
+		const token = sign({ sub: user._id }, config.jwtSecret, {
+			expiresIn: '7d',
+			algorithm: 'HS256',
+		});
+
+		// Respond with token
+		res.status(201).json({ accessToken: token });
+	} catch {
+		return next(createHttpError(500, 'Error while generating token'));
+	}
+};
+
+export { createUser, loginUser };
