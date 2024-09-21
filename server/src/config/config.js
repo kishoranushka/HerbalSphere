65af1f51ff1ed575fd854d2c71c7e8c79519784b
Abhishek Mewada <abhishek.engineer3@gmail.com>
2024-09-21 12:57:21 +0530

    setup global error handler

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -4,6 +4,7 @@ conf();
 const _config = {
 	port: process.env.PORT,
 	databaseUrl: process.env.ATLASDB_URL,
+	env: process.env.NODE_ENV,
 };
 
 export const config = Object.freeze(_config); // freeze -> read only
--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,22 @@
 import express from 'express';
 import { config } from './config/config.js';
 import connectDB from './config/db.js';
+import globalErrorHandler from './middleware/globalErrorHandler.js';
+import createHttpError from 'http-errors';
 
 const app = express();
 
 // connect to database
 connectDB();
+
+app.get('/', () => {
+	const error = createHttpError(400, 'Something went wrong');
+	throw error;
+	console.log('Running home');
+});
+
+// Global error handler
+app.use(globalErrorHandler);
 
 const PORT = config.port || 3000;
 
